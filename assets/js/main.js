3981dbf7c671543f5fba472abe2c777bcb8ee12c
Erland Ruiz <erlandruiz@hotmail.com>
2023-04-07 11:27:39 -0500

    First commit Home and New section

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// Change Background Header
+
+const scrollHeader = ()=>{
+    const header = document.getElementById('header');
+    this.scrollY >=50   ? header.classList.add('scroll-header')
+                        : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
