0caa3f010c0ea86dd19be50b7781f45e538ab14a
Erland Ruiz <erlandruiz@hotmail.com>
2023-04-07 16:16:44 -0500

    second commit men and women collections, brands , best products , brands

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,21 @@ const scrollHeader = ()=>{
                         : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+// Swiper Products
+let swiperProducts = new Swiper(".products__container", {
+    spaceBetween:32,
+    grabCursor:true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1024: {
+        spaceBetween: 72,
+      },
+    },
+  });
